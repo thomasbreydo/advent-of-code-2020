e0597d9eb5396e36f27fcd327f88f3adc91641c3
Thomas Breydo <tbreydo@gmail.com>
2020-12-04 09:03:42 -0500

    Optimize solution to day 1 part 2

--- 1/part2.py
+++ 1/part2.py
@@ -6,7 +6,7 @@ N_NUMBERS = 3
 INPUT = "input2_from_max.txt"
 
 with open(INPUT) as f:
-    report = [int(n) for n in f.readlines()]
+    report = {int(n) for n in f.readlines()}
 
 start = time.time()
 for list_of_n in itertools.permutations(report, N_NUMBERS - 1):
