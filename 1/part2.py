35145f83d48b77ec4bdc5c803c754368eac8db1f
Thomas Breydo <tbreydo@gmail.com>
2020-12-04 09:15:17 -0500

    Optimize solution for part 2 of day 1

--- 1/part2.py
+++ 1/part2.py
@@ -8,6 +8,7 @@ INPUT = "input2_from_max.txt"
 with open(INPUT) as f:
     report = {int(n) for n in f.readlines()}
 
+
 start = time.time()
 for list_of_n in itertools.permutations(report, N_NUMBERS - 1):
     if (final_n := 2020 - sum(list_of_n)) in report and final_n not in list_of_n:
