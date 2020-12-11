ebf66b660abc41b5a904ce48002acfa1a4a4c2d4
Thomas Breydo <tbreydo@gmail.com>
2020-12-11 17:02:26 -0500

    Solve part 1 of day 9

---
+++ 9/part1.py
@@ -0,0 +1,27 @@
+from collections import deque
+from typing import Iterable
+
+INPUT_FILE: str = "input.txt"
+
+
+def two_sum_to_target(numbers: Iterable[int], target: int) -> bool:
+    number: int
+    for number in numbers:
+        if target - number in numbers:
+            if number != target / 2:  # the two addends must be different
+                return True
+    return False
+
+
+def first_invalid(transmission: Iterable[int], preamble_len: int = 25) -> int:
+    previous: deque = deque(transmission[:25], maxlen=preamble_len)
+    for n in transmission[25:]:
+        if not two_sum_to_target(previous, n):
+            return n
+        previous.append(n)
+    raise ValueError("no invalid transmissions")
+
+
+if __name__ == "__main__":
+    with open(INPUT_FILE) as f:
+        print(first_invalid([int(x) for x in f.read().split("\n")]))
